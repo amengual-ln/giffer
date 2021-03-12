50c941438fb9c3c6eb5262cc9360a0042d774692
Lautaro Amengual <amengual.ln@gmail.com>
2021-03-12 02:27:56 -0300

    infinite scroll implemented

--- src/pages/GifResults/index.js
+++ src/pages/GifResults/index.js
@@ -1,23 +1,38 @@
-import React from 'react'
-import GifList from '../../components/GifList'
-import Loading from '../../components/Loading'
-import useGifs from '../../hooks/useGifs'
+import React, { useRef, useEffect, useMemo } from "react";
+import GifList from "../../components/GifList";
+import Loading from "../../components/Loading";
+import useGifs from "../../hooks/useGifs";
+import useNearScreen from "../../hooks/useNearScreen";
+import throttle from "just-throttle"
 
-export default function GifResults ({ params }) {
-    const { keyword } = params
-    const { loading, gifs, setPage } = useGifs({ keyword })
+export default function GifResults({ params }) {
+  const { keyword } = params;
+  const { loading, gifs, setPage } = useGifs({ keyword });
+  const externalRef = useRef();
+  const {isNearScreen} = useNearScreen({
+    externalRef: loading ? null : externalRef,
+    once: false
+  });
 
-    const handleNextPage = () => setPage(prevPage => prevPage + 1)
+  const throttleHandleNextPage = useMemo(() =>
+    throttle(() => setPage((prevPage) => prevPage + 1), 200, true),
+    [setPage]
+  );
+ 
+  useEffect(function () {
+    if (isNearScreen) throttleHandleNextPage(); 
+}, [throttleHandleNextPage, isNearScreen]);
 
-    return <>
-    {
-        loading 
-        ? <Loading /> 
-        : <>
-            <GifList gifs={gifs} />
-            <br />
-            <button onClick={handleNextPage}>Get more!</button>
+  return (
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <GifList gifs={gifs} />
+          <div id="visor" ref={externalRef}></div>
         </>
-    }
+      )}
     </>
+  );
 }
